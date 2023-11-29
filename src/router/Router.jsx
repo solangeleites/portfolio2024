ce3149fff5bd4cd6d54d073ae4e97a2555974019
solange <solangeleitesg@gmail.com>
2023-11-29 20:00:12 -0300

    modifique una ruta mal escrita

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../components/Layout/Layout'
+import Layout from '../components/layout/Layout'
 import Home from '../pages/home/Home'
 
 const Router = () => {
@@ -9,8 +9,6 @@ const Router = () => {
         <Layout />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/projects" element={<Projects />} /> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
   )
